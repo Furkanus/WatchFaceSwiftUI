b9a624a7e386fae3d5b5d5a2ceba2afe06baff96
Furkan Hanci <60580341+Furkanus@users.noreply.github.com>
2020-09-19 12:32:12 +0300

    Add files via upload

---
+++ WatchSwiftUI/WatchSwiftUI/WatchSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  WatchSwiftUIApp.swift
+//  WatchSwiftUI
+//
+//  Created by Furkan Hanci on 9/19/20.
+//
+
+import SwiftUI
+
+@main
+struct WatchSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
